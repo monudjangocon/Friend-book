f5601c841e23327f56b9a903a021ad4d514aa962
monudjangocon <djangopycon@gmail.com>
2016-03-16 14:38:43 +0530

    Update models.py

--- prof/models.py
+++ prof/models.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import os.path
 
 class Profile(models.Model):
