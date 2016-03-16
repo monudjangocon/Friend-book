4f8a21b76fdc648a6f7cc57ec1086f7951a338e6
monudjangocon <djangopycon@gmail.com>
2016-03-16 14:43:58 +0530

    Update views.py

--- prof/views.py
+++ prof/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect,render_to_response
+from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
 from prof.forms import SignUpForm
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 def signup(request):
+    ## here post request.....
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if not form.is_valid():
-            return render(request, 'signup.html', {'form': form})
+            return render_to_response('signup.html', {'form': form},context_instance=RequestContext(request))
         else:
             username = form.cleaned_data.get('username')
             email = form.cleaned_data.get('email')
@@ -15,9 +18,11 @@ def signup(request):
             User.objects.create_user(username=username, password=password, email=email)
             user = authenticate(username=username, password=password)## user has been added to the current session
             login(request, user)
-            return redirect('/')
+            return HttpResponseRedirect('/')
+    ## get request......        
     else:
-        return render(request, 'signup.html', {'form': SignUpForm()})
+        form=SignUpForm()
+        return render_to_response( 'signup.html', {'form':form},context_instance=RequestContext(request))
 
 def Cover(request):
-    return render_to_response("cover.html",{})
+    return render_to_response("cover.html",{},context_instance=RequestContext(request))
