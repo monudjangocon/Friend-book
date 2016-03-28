35069da4c1e530d7074875a82166f26169648160
monudjangocon <djangopycon@gmail.com>
2016-03-28 12:30:17 -0700

    Update models.py

--- notify/models.py
+++ notify/models.py
@@ -5,15 +5,16 @@ from django.utils.html import escape
 class Notify(models.Model):
  
     NOTIFY_TYPES = (
-        
-        ('LIKE', 'Like'),
         ('FAVORITE', 'Favorite'),
+        ('LIKE', 'Like'),
+        
         )
 
     user = models.ForeignKey(User)
     notify_type = models.CharField(max_length=1, choices=NOTIFY_TYPES)
-    date = models.DateTimeField(auto_now_add=True)
     feed = models.IntegerField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True,auto_now=False)
+  
 
  
 
@@ -37,36 +38,32 @@ class Notification(models.Model):
     notification_type = models.CharField(max_length=1, choices=NOTIFICATION_TYPES)
     is_read = models.BooleanField(default=False)
 
-    _LIKED_TEMPLATE = u'<a href="/{0}/">{1}</a> liked your post: <a href="/feeds/{2}/">{3}</a>'
-    _COMMENTED_TEMPLATE = u'<a href="/{0}/">{1}</a> commented on your post: <a href="/feeds/{2}/">{3}</a>'
+    liked= u'<a href="/{0}/">{1}</a> liked your post: <a href="/feeds/{2}/">{3}</a>'
+    comment= u'<a href="/{0}/">{1}</a> commented on your post: <a href="/feeds/{2}/">{3}</a>'
    
     
 
     def __unicode__(self):
         if self.notification_type == self.LIKED:
-            return self._LIKED_TEMPLATE.format(
+            return self.liked.format(
                 escape(self.from_user.username),
                 escape(self.from_user.profile.get_screen_name()),
                 self.feed.pk,
-                escape(self.get_summary(self.feed.post))
+                escape(self.item(self.feed.post))
                 )
         elif self.notification_type == self.COMMENTED:
-            return self._COMMENTED_TEMPLATE.format(
+            return self.comment.format(
                 escape(self.from_user.username),
                 escape(self.from_user.profile.get_screen_name()),
                 self.feed.pk,
-                escape(self.get_summary(self.feed.post))
+                escape(self.item(self.feed.post))
                 )
-      
-       
-        
-        
-        else:
-            return 'Ooops! Something went wrong.'
+         else:
+            return 'Ooops! Something went wrong. ckeck it'
 
-    def get_summary(self, value):
-        summary_size = 50
-        if len(value) > summary_size:
-            return u'{0}...'.format(value[:summary_size])
+    def item(self, value):
+        temp= 50
+        if len(value) > temp:
+            return u'{0}...'.format(value[:temp])
         else:
             return value
