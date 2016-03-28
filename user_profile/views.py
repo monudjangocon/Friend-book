ccea8212f534041618286de7870ca9617ceafa78
monudjangocon <djangopycon@gmail.com>
2016-03-28 12:59:55 -0700

    Update views.py

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,24 +1,22 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.contrib import messages
+from PIL import Image
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings as django_settings
-from django.shortcuts import render, redirect, get_object_or_404
 from feed.models import Feed
-from feed.views import FEEDS_NUM_PAGES
 from feed.views import feeds
-import os
-from PIL import Image
+
 from user_profile.forms import ProfileForm, ChangePasswordForm
-
-
+from django.contrib.auth.decorators import login_required
+import os
 
 @login_required
 def profile(request, username):
     user=request.user
     page_user = get_object_or_404(User, username=username)
     all_feeds = Feed.get_feeds().filter(user=page_user)
-    paginator = Paginator(all_feeds, FEEDS_NUM_PAGES)
+    paginator = Paginator(all_feeds,8)
     feeds = paginator.page(1)
     from_feed = -1
     if feeds:
@@ -42,12 +40,12 @@ def settings(request):
             user.email = form.cleaned_data.get('email')
             user.profile.location = form.cleaned_data.get('location')
             user.save()
-            messages.add_message(request, messages.SUCCESS, 'Your profile were successfully edited.')
+            messages.add_message(request, messages.SUCCESS, 'Your profile has been  successfully edited.')
     else:
         form = ProfileForm(instance=user, initial={
             'location': user.profile.location
             })
-    return render(request, 'setting.html', {'form':form})
+    return render_to_response('setting.html', {'form':form})
 
 @login_required
 def picture(request):
@@ -57,7 +55,7 @@ def picture(request):
             uploaded_picture = True
     except Exception, e:
         pass
-    return render(request, 'picture.html', {'uploaded_picture': uploaded_picture})
+    return render_to_response('picture.html', {'uploaded_picture': uploaded_picture})
 
 @login_required
 def password(request):
@@ -68,52 +66,13 @@ def password(request):
             new_password = form.cleaned_data.get('new_password')
             user.set_password(new_password)
             user.save()
-            messages.add_message(request, messages.SUCCESS, 'Your password were successfully changed.')
+            messages.add_message(request, messages.SUCCESS, 'Your password has been  successfully changed....')
     else:
         form = ChangePasswordForm(instance=user)
-    return render(request, 'password.html', {'form':form})
+    return render_to_response('password.html', {'form':form})
 
-@login_required
-def upload_picture(request):
-    try:
-        profile_pictures = django_settings.MEDIA_ROOT + '/profile_pictures/'
-        if not os.path.exists(profile_pictures):
-            os.makedirs(profile_pictures)
-        f = request.FILES['picture']
-        filename = profile_pictures + request.user.username + '_tmp.jpg'
-        with open(filename, 'wb+') as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)    
-        im = Image.open(filename)
-        width, height = im.size
-        if width > 350:
-            new_width = 350
-            new_height = (height * 350) / width
-            new_size = new_width, new_height
-            im.thumbnail(new_size, Image.ANTIALIAS)
-            im.save(filename)
-        return redirect('/setting/picture/?upload_picture=uploaded')
-    except Exception, e:
-        return redirect('/setting/picture/')
 
-@login_required
-def save_uploaded_picture(request):
-    try:
-        x = int(request.POST.get('x'))
-        y = int(request.POST.get('y'))
-        w = int(request.POST.get('w'))
-        h = int(request.POST.get('h'))
-        tmp_filename = django_settings.MEDIA_ROOT + '/profile_pictures/' + request.user.username + '_tmp.jpg'
-        filename = django_settings.MEDIA_ROOT + '/profile_pictures/' + request.user.username + '.jpg'
-        im = Image.open(tmp_filename)
-        cropped_im = im.crop((x, y, w+x, h+y))
-        cropped_im.thumbnail((200, 200), Image.ANTIALIAS)
-        cropped_im.save(filename)
-        os.remove(tmp_filename)
-    except Exception, e:
-        pass
-    return redirect('/setting/picture/')
 
 def log_out(request):
     user=request.user
-    return render(request,'core/logout.html',{'user':user})
+    return render_to_response('core/logout.html',{'user':user})
