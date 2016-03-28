3c33c3b62167bc09672db664f902b39978367e82
monudjangocon <djangopycon@gmail.com>
2016-03-28 11:13:27 -0700

    Update forms.py

--- prof/forms.py
+++ prof/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 
 
 def InvalidUsernameValidator(value):
