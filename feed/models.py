1562f102bb89b88ab1098d04b96d223978e43321
monudjangocon <djangopycon@gmail.com>
2016-03-28 13:06:09 -0700

    Update models.py

--- feed/models.py
+++ feed/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from notify.models import Notify
 from django.utils.html import escape
 
@@ -8,21 +8,18 @@ from django.utils.html import escape
 
 class Feed(models.Model):
     user = models.ForeignKey(User)
-    date = models.DateTimeField(auto_now_add=True)
     post = models.TextField(max_length=255)
     parent = models.ForeignKey('Feed', null=True, blank=True)
     likes = models.IntegerField(default=0)
-    comments                                                                                                                                         = models.IntegerField(default=0)
-
-    class Meta:
-        verbose_name = _('Feed')
-        verbose_name_plural = _('Feeds')
-        ordering = ('-date',)
+    comments=models.CharField(max_length=100,null=True, blank=True) 
+    date = models.DateTimeField(auto_now_add=True)
+   
+                                                                                                                                           = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.post
 
-    @staticmethod
+    @staticmethod ## accessing member function using name of class
     def get_feeds(from_feed=None):
         if from_feed is not None:
             feeds = Feed.objects.filter(parent=None, id__lte=from_feed)
@@ -67,5 +64,5 @@ class Feed(models.Model):
         self.save()
         return feed_comment
 
-    def linkfy_post(self):
+    def linked(self):
         return bleach.linkify(escape(self.post))
