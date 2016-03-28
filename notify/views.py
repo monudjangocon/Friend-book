466065492ae05493aeef731aa6b4b26589121c92
monudjangocon <djangopycon@gmail.com>
2016-03-28 12:34:44 -0700

    Update views.py

--- notify/views.py
+++ notify/views.py
@@ -1,29 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from notify.models import Notification
-from friend.decorators import ajax_required
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from friend.decorators import ajax_required
 
-
+## checking user login or not....
 @login_required
 def notifications(request):
     user = request.user
     notifications = Notification.objects.filter(to_user=user)
-    unread = Notification.objects.filter(to_user=user, is_read=False)
-    for notification in unread:
+    not_read= Notification.objects.filter(to_user=user, is_read=False)
+    for notification in not_read:
         notification.is_read = True
         notification.save()        
-    return render(request, 'notification.html', {'notifications': notifications})
+    return render_to_respone('notification.html', {'notifications': notifications})
 
 @login_required
 @ajax_required
 def last_notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(to_user=user, is_read=False)[:5]
+    user = request.user## logged in user
+    notifications = Notification.objects.filter(to_user=user, is_read=False)[:5] ## we 're fetching ist five user 
     for notification in notifications:
         notification.is_read = True
         notification.save()
-    return render(request, 'last_notifications.html', {'notifications': notifications})
+    return render_to_response('last_notifications.html', {'notifications': notifications})
 
 @login_required
 @ajax_required
